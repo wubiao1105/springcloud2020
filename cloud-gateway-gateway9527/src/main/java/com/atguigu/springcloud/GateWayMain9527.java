d4cb934b1467fd61a9d2fb1b9bba86d3469cd5ca
wb <1342626556@qq.com>
2020-04-25 13:56:26 +0800

    搭建Getway网关微服务

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GateWayMain9527.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Description 路由主启动类
+ * @Author wb
+ * @Date 2020/4/25 13:37
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
