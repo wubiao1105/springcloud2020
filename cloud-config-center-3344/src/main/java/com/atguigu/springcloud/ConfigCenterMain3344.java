a2e62990af9dbdb04ec487c365ce95164dfd9ad8
wb <1342626556@qq.com>
2020-04-25 16:56:56 +0800

    config-配置中心服务端搭建

---
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Description 分布式配置中心Config主启动类
+ * @Author wb
+ * @Date 2020/4/25 16:44
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
