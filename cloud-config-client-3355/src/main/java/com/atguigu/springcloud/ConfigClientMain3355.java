16c007a9ef908bbd68fa9d100b13e3d247044e1c
wb <1342626556@qq.com>
2020-05-02 17:41:45 +0800

    config-配置中心客户端搭建

--- cloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
@@ -2,19 +2,17 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
- * @Description 分布式配置中心Config主启动类
+ * @Description config客户端主启动类
  * @Author wb
- * @Date 2020/4/25 16:44
+ * @Date 2020/5/2 16:14
  */
 @SpringBootApplication
-@EnableConfigServer
 @EnableEurekaClient
-public class ConfigCenterMain3344 {
+public class ConfigClientMain3355 {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigCenterMain3344.class, args);
+        SpringApplication.run(ConfigClientMain3355.class, args);
     }
 }
